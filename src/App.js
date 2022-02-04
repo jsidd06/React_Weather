65f2ebe2800143114e092eed93a5ecf24d906c4e
siddharth <jsidd06@gmail.com>
2022-02-04 14:58:19 +0530

    change the icon and color

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from './views/HomeScreen';
-
 function App() {
   return (
+    <>
       <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<HomeScreen/>} />
-      </Routes>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomeScreen />}
+          />
+        </Routes>
       </BrowserRouter>
-  )
+    </>
+  );
 }
 
 export default App;
--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-import React, { Fragment, useState } from "react";
-import { Container, Input, Button, Card, Col, Row } from "reactstrap";
-
+import React, { useState } from "react";
+import { Container, Input, Button, Card, } from "reactstrap";
 function HomeScreen() {
   // weather app state
   const [weather, setWeather] = useState(null);
@@ -22,16 +21,20 @@ function HomeScreen() {
   };
   return (
     <Container style={{ textAlign: "center" }} className="mt-5">
-      <h1>weather</h1>
+      <h1>Weather 🌦</h1>
+      {/* Add the state here [setcity]*/}
       <Input
-        placeholder="Enter your city"
+        className="mt-3"
+        placeholder="Enter Your City Name"
         onChange={(e) => setCity(e.target.value)}
       />
+      {/* Add the value of data here  {getweather}*/}
       <Button type="submit" className="mt-3 mb-3" onClick={getWeather}>
         Check Now
       </Button>
+      {/* set api Data here */}
       {weather && (
-        <Card className="mt-3 p-3">
+        <Card className="mt-3 p-3" style={{ backgroundColor: "#96CEB4" }}>
           <h3>City Name :- {weather.name}</h3>
           <h3>Weather :- {weather.weather[0].main}</h3>
           <h3>Description :- {weather.weather[0].description}</h3>
@@ -42,9 +45,8 @@ function HomeScreen() {
           <h3>Humidity :- {weather.main.humidity}</h3>
           <h3>Wind Speed :- {weather.wind.speed}</h3>
           <h3>Clouds :- {weather.clouds.all}</h3>
-          <h3>Sunrise :- {weather.sys.sunrise}</h3> 
+          <h3>Sunrise :- {weather.sys.sunrise}</h3>
           <h3>Sunset :- {weather.sys.sunset}</h3>
-
         </Card>
       )}
     </Container>
@@ -52,12 +54,3 @@ function HomeScreen() {
 }
 
 export default HomeScreen;
-
-    /* {weather && (
-        <Fragment>
-          <p>{weather.main.temp}</p>
-          <span>{weather.weather[0].icon}</span>
-          <p>{weather.sys.country}</p>
-          <p>{weather.wind.speed}</p>
-        </Fragment>
-      )} */
